14fea88566ece21a1ce51263e4a10f54133fe223
Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>
2024-05-27 16:33:26 +0530

    updated javadoc
    Signed-off-by: Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoServiceImpl.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoServiceImpl.java
@@ -1005,7 +1005,7 @@ public class IdRepoServiceImpl implements IdRepoService<IdRequestDTO, Uin> {
 	 * @param request IdRequestDTO
 	 * @param uinHash
 	 * @param method
-	 * @return
+	 * @return handles map
 	 * @throws IdRepoAppException
 	 */
 	private Map<String, HandleDto> checkAndGetHandles(IdRequestDTO request, String uinHash, String method)
