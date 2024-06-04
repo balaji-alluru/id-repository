ea165cbc3f10db927251142de9b4e9fbb53b38a1
Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>
2024-06-04 17:56:01 +0530

    testing issue with credentialrequeststatus table
    Signed-off-by: Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoServiceImpl.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoServiceImpl.java
@@ -939,14 +939,14 @@ public class IdRepoServiceImpl implements IdRepoService<IdRequestDTO, Uin> {
 				credStatus.setStatus(CredentialRequestStatusLifecycle.NEW.toString());
 				credStatus.setUpdatedBy(IdRepoSecurityManager.getUser());
 				credStatus.setUpdDTimes(DateUtils.getUTCCurrentDateTime());
-				credRequestRepo.save(credStatus);
+				credRequestRepo.saveAndFlush(credStatus);
 			});
 		} else if (!credStatusList.isEmpty() && !uinStatus.contentEquals(activeStatus)) {
 			credStatusList.forEach(credStatus -> {
 				credStatus.setStatus(CredentialRequestStatusLifecycle.DELETED.toString());
 				credStatus.setUpdatedBy(IdRepoSecurityManager.getUser());
 				credStatus.setUpdDTimes(DateUtils.getUTCCurrentDateTime());
-				credRequestRepo.save(credStatus);
+				credRequestRepo.saveAndFlush(credStatus);
 			});
 		} else if (credStatusList.isEmpty()) {
 			CredentialRequestStatus credStatus = new CredentialRequestStatus();
@@ -960,8 +960,8 @@ public class IdRepoServiceImpl implements IdRepoService<IdRequestDTO, Uin> {
 			credStatus.setCrDTimes(DateUtils.getUTCCurrentDateTime());
 			if(enableConventionBasedId && (requestId != null)) {
 				credStatus.setRequestId(requestId);
-			}
-			credRequestRepo.save(credStatus);
+			} 
+			credRequestRepo.saveAndFlush(credStatus);
 		}
 	}
 
